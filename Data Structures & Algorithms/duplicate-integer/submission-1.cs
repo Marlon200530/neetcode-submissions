2a05a10294e46be85a2325dbe8f05f65851af8d5
Marlon S. Nhantumbo <128413802+Marlon200530@users.noreply.github.com>
2026-04-21 10:57:04 +0200

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        var map = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            if (map.Contains(nums[i])) {
+                return true;
+            }
+
+            map.Add(nums[i]);
+        }
+
+        return false;
+    }
+}
